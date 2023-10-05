6ee8acb836993c04a58a0dec44fe81d918202566
mac01 <mac01@gmail.com>
2023-10-05 18:37:32 +0800

    api日志

--- src/main/java/cn/bensun/elasticsearch/mq/LogConsumer.java
+++ src/main/java/cn/bensun/elasticsearch/mq/LogConsumer.java
@@ -3,17 +3,21 @@ package cn.bensun.elasticsearch.mq;
 import cn.bensun.elasticsearch.domain.LogMsg;
 import cn.bensun.elasticsearch.mapper.LogMsgRepository;
 import cn.bensun.elasticsearch.util.DateUtil;
-import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 @Slf4j
 @Component
 public class LogConsumer {
     @Autowired
     private LogMsgRepository logMsgRepository;
+    //    "requestId":"%X{requestId:-${setRequestId}}","deviceName":"${deviceName}","createdTime":"%d{yyyy-MM-dd HH:mm:ss.SSS}","level":"%-5level","method":"%logger{full} - [%method,%line]","msg":"%msg"
+    private static Pattern pattern = Pattern.compile("requestId:\":");
 
     @RabbitHandler
     @RabbitListener(
@@ -26,17 +30,17 @@ public class LogConsumer {
     public void receiveMessage(String message) {
         // 处理收到的消息逻辑
         try {
-            JSONObject jsonObject = JSONObject.parseObject(message);
+            String[] split = message.split("====");
             LogMsg logMsg = new LogMsg();
-            logMsg.setCreatedTime(DateUtil.str2Time(jsonObject.getString("createdTime")).getTime());
-            logMsg.setRequestId(jsonObject.getString("requestId"));
-            logMsg.setDeviceName(jsonObject.getString("deviceName"));
-            logMsg.setLevel(jsonObject.getString("level"));
-            logMsg.setMethod(jsonObject.getString("method"));
-            logMsg.setMsg(jsonObject.getString("msg"));
+            logMsg.setCreatedTime(Objects.requireNonNull(DateUtil.str2Time(split[2])).getTime());
+            logMsg.setRequestId(split[0]);
+            logMsg.setDeviceName(split[1]);
+            logMsg.setLevel(split[3]);
+            logMsg.setMethod(split[4]);
+            logMsg.setMsg(split[5]);
             logMsgRepository.save(logMsg);
         } catch (Exception e) {
-            log.error("日志异常\t{}",message);
+            log.error("日志异常\t{}", message);
         }
     }
 }
