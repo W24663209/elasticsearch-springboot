db24ef25286aa7e5a379b22f3d1854d5b44a267e
mac01 <mac01@gmail.com>
2023-10-30 13:56:46 +0800

    调整

--- release/es.sh
+++ release/es.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-FILE_NAME=/Users/weizongtang/IdeaProjects/elasticsearch-springboot/target/elasticsearch-0.0.1-SNAPSHOT.jar
-scp -i ~/.ssh/website.pem $FILE_NAME  ubuntu@35.154.48.78:/home/ubuntu/elasticsearch-docker/api
+mvn clean install -Dmaven.test.skip
+FILE_NAME=/Users/mac01/IdeaProjects/elasticsearch-springboot/target/elasticsearch-0.0.1-SNAPSHOT.jar
+scp  $FILE_NAME  ubuntu@100.121.132.18:/home/ubuntu/elasticsearch-docker/api
