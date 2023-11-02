374c2a03a5d3f959b329dbc7322ec5fecbb813f0
mac01 <mac01@gmail.com>
2023-11-02 17:42:49 +0800

    调整

--- release/es.sh
+++ release/es.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+mvn clean install -Dmaven.test.skip
 FILE_NAME=/Users/mac01/IdeaProjects/elasticsearch-springboot/target/elasticsearch-0.0.1-SNAPSHOT.jar
-scp -i /Users/mac01/OtPay/jpay-server/cers/kingpay.pem $FILE_NAME  ubuntu@3.110.181.58:/home/ubuntu/es-api
+scp  $FILE_NAME  ubuntu@13.212.125.188:/home/ubuntu/es-api
