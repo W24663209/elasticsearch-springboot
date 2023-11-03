5674f906653cbe3bead29cbbba38c2a27a12c11f
weizongtang <24663209@qq.com>
2023-11-04 02:39:21 +0800

    1

--- release/es.sh
+++ release/es.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mvn clean install -Dmaven.test.skip
-FILE_NAME=/Users/mac01/IdeaProjects/elasticsearch-springboot/target/elasticsearch-0.0.1-SNAPSHOT.jar
+FILE_NAME=$(pwd)/target/elasticsearch-0.0.1-SNAPSHOT.jar
 scp  $FILE_NAME  ubuntu@100.84.20.133:/home/ubuntu/elasticsearch-docker/api
