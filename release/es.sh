746ecc908864c484f3fb42777f202a4293611bfa
weizongtang <24663209@qq.com>
2023-11-27 01:52:05 +0800

    1

--- release/es.sh
+++ release/es.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mvn clean install -Dmaven.test.skip
 FILE_NAME=$(pwd)/target/elasticsearch-0.0.1-SNAPSHOT.jar
-scp  $FILE_NAME  ubuntu@100.84.20.133:/home/ubuntu/elasticsearch-docker/api
+scp  $FILE_NAME  ecs-user@8.219.104.130:/home/ecs-user/elasticsearch-docker/api
